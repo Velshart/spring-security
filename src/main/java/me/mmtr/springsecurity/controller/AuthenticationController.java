6810980692e7d15f103bad0778ddd32d95bef10c
Velshh <pkot2003@gmail.com>
2024-08-21 22:43:15 +0200

    Initial commit

---
+++ src/main/java/me/mmtr/springsecurity/controller/AuthenticationController.java
@@ -0,0 +1,69 @@
+package me.mmtr.springsecurity.controller;
+
+import me.mmtr.springsecurity.data.User;
+import me.mmtr.springsecurity.data.UserDTO;
+import me.mmtr.springsecurity.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class AuthenticationController {
+    private final UserService userService;
+
+    public AuthenticationController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/register")
+    public String registrationForm(Model model) {
+        UserDTO userDTO = new UserDTO();
+        model.addAttribute("user", userDTO);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute("user") UserDTO userDTO, BindingResult bindingResult, Model model) {
+        User existingUser = userService.findUserByUsername(userDTO.getUsername());
+
+        if (existingUser != null && existingUser.getUsername() != null && !existingUser.getUsername().isEmpty()) {
+            bindingResult.rejectValue("username", "exists", "This username is already in use");
+        }
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("user", userDTO);
+            return "register";
+        }
+
+        userService.saveUser(userDTO);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<UserDTO> userDTOs = userService.findAllUsers();
+
+        model.addAttribute("users", userDTOs);
+        return "users";
+    }
+}
