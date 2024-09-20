d5a21a14e7892a0ce74b6a827bfab17da4b57d5b
Velshh <pkot2003@gmail.com>
2024-09-20 20:41:50 +0200

    minor fix

--- src/main/java/me/mmtr/springsecurity/data/User.java
+++ src/main/java/me/mmtr/springsecurity/data/User.java
@@ -21,7 +21,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
